966e0b74bf32253cd93f4f32f50945420c71ab17
Nguyễn Vinh Cường <cuongnv.developer@gmail.com>
2021-01-18 15:51:27 +0700

    Init package

---
+++ src/LarabServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CuongDev\Larab;
+
+use Illuminate\Support\ServiceProvider;
+
+class LarabServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
